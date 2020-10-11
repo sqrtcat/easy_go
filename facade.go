69a1fe72c38b0de4deba8030eeaf2fc69e365e95
sqrtcat <32448732@qq.com>
2020-10-11 16:52:08 +0800

    引入第三方包

--- facade.go
+++ facade.go
@@ -1,9 +1,15 @@
 package easy_go
 
 import (
+	"fmt"
 	"github.com/sqrtcat/easy_go/lib"
+	"github.com/startdusk/goutil/hash"
 )
 
 func Hello() {
 	lib.Hello()
+}
+
+func Md5() {
+	fmt.Println(hash.StringMd5("hello go"))
 }
